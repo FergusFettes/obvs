dced83e1743772311887686ba4224088e5936445
fergus <fergusfettes@gmail.com>
2024-01-29 17:02:10 +0530

    fix imports

--- patchscopes_nnsight/patchsopes_logit_lens.py
+++ patchscopes_nnsight/patchsopes_logit_lens.py
@@ -1,10 +1,10 @@
-from patchscopes_pytorch.patchsopes import SourceContext, TargetContext, Patchscope
+from patchscopes_nnsight.patchsopes import SourceContext, TargetContext, Patchscope
 
 prompt = "Today is opposites day. The grass is blue. The sea is green. Our protagonist goes outside and looks around at the birds and the beas. She looks up at the sky, and marvels at its hue, a beautiful bright green. The suns color is"
 
 # Setup source and target context with the simplest configuration
 source_context = SourceContext(
-    input_sequence=[prompt],  # Example input text
+    input_sequence=prompt,  # Example input text
     model_name="gpt2",
     position=-1,  # Last token (assuming single input)
     layer=0,  # 10th layer (logit lense actually tests each layer, we'll start with one.)
